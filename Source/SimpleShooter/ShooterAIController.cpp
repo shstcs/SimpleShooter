cdc547b8c2a59b8e8b2fb9652c15c95f03e37e10
shstcs <shstcs@naver.com>
2025-03-21 17:59:48 +0900

    [188,9] BT

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -15,13 +15,14 @@ void AShooterAIController::BeginPlay()
 {
     Super::BeginPlay();
     PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-    // SetFocus(PlayerPawn);
 
     if(AIBehavior != nullptr)
     {
         RunBehaviorTree(AIBehavior);
 
         GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
     }
     else
     {
@@ -34,16 +35,13 @@ void AShooterAIController::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
 
-    // // if Line of Sight
-    // if(LineOfSightTo(PlayerPawn))
-    // {
-    //     MoveToActor(PlayerPawn, AcceptanceRadius);
-    //     SetFocus(PlayerPawn);
-    //     // fire
-    // }
-    // else
-    // {
-    //     ClearFocus(EAIFocusPriority::Gameplay);
-    //     // stop firing
-    // }
+    if(LineOfSightTo(PlayerPawn))
+    {
+        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+    }
+    else
+    {
+        GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+    }
 }
